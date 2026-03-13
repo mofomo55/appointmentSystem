76bef1d43af32adc598637847c3245bccd0e2d71
Mohamad Eibo <Mohamad Eibo@DESKTOP-1UQ8JGM>
2026-03-13 15:23:50 +0100

    Connection with Database + packets update and renew the projects structure

---
+++ appointmentSystem/AppointmentBooking.Domains/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentBooking.Domains.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email  { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Role { get; set; }
+
+
+    }
+}
---
+++ appointmentSystem/AppointmentBooking.Persistencee/config/PersistenceServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using AppointmentBooking.Persistencee.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentBooking.Persistencee.config
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseMySql(
+                    connectionString,
+                    ServerVersion.AutoDetect(connectionString)
+                ));
+
+            return services;
+        }
+    }
+}
